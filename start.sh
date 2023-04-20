00716ae520774ee3803c66bd16a87f1f5d027454
温建锋 <wenjianfeng@cloudwalk.com>
2023-04-20 10:44:14 +0800

    update

---
+++ start.sh
@@ -0,0 +1,7 @@
+export CUDA_VISIBLE_DEVICES=0,1
+export PYTHONPATH="/vehicle/yckj3860/code/stable-diffusion/:/vehicle/yckj3860/code/stable-diffusion/taming-transformers:/vehicle/yckj3860/code/CLIP_wjf"
+
+# /vehicle/yckj3860/miniconda3/envs/py37_torch1_7/bin/python scripts/txt2img.py --prompt "a photograph of an astronaut riding a horse" --plms 
+
+# try diffuser
+/vehicle/yckj3860/miniconda3/envs/py37_torch1_7/bin/python my_try_diffuser.py
